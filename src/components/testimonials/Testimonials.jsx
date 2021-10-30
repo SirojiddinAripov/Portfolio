31006b014d10af35b9f38694d4ea72b3f06c3fc3
Sam Aripov <aripov86cm@gmail.com>
2021-10-30 15:17:09 -0400

    Slideshows are functional, animations are added, no text support atm

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import "./testimonials.scss"
-import lara from '../../images/lara.png'
+import one from '../../images/fallout.png'
+import two from '../../images/lara.png'
+import three from '../../images/portal.png'
+import four from '../../images/ruiner.png'
+
+let id = 0;
 
 export default function Testimonials() {
     return (
         <div className="testimonials" id="testimonials">
 
             <div className="slides3">
-                <img src={lara} alt="" />
+                <img src={one} alt="" id="imagePan2"/>
                 <div className="buttons3">
-                    <button className="button3 left3" style={{ float: "left", }} onClick={() => console.log("left")}>⮜</button>
-                    <button className="button3 right3" style={{ float: "right", }} onClick={() => console.log("right")}>⮞</button>
+                    <button className="button3 left3" style={{ float: "left", }} onClick={() => { id--; changePic("imagePan2"); }}>⮜</button>
+                    <button className="button3 right3" style={{ float: "right", }} onClick={() => { id++; changePic("imagePan2"); }}>⮞</button>
                 </div>
             </div>
 
@@ -18,7 +23,35 @@ export default function Testimonials() {
                 <h1>Engineering Projects</h1>
                 <p>These are my engineering projects because a quick brown fox jumps over the lazy dog. A quick brown fox jumps over the lazy dog. A quick brown fox jumps over the lazy dog</p>
             </div>
-            
+
         </div>
     )
+}
+
+let changePic = (imageID) => {
+    if (id > 3)
+        id = 0;
+    else if (id < 0)
+        id = 3;
+    document.querySelector("#" + imageID).style = "opacity: 5%"
+    setTimeout(() => {
+        if (id === 0) {
+
+            document.querySelector("#" + imageID).src = one;
+
+        } else if (id === 1) {
+
+            document.querySelector("#" + imageID).src = two;
+
+        } else if (id === 2) {
+
+            document.querySelector("#" + imageID).src = three;
+
+        } else if (id === 3) {
+
+            document.querySelector("#" + imageID).src = four;
+
+        }
+    }, 200)
+    setTimeout(() => document.querySelector("#" + imageID).style = "opacity: 100%", 250);
 }
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./works.scss"
-import fall from '../../images/fallout.png'
-import lara from '../../images/lara.png'
-import portal from '../../images/portal.png'
-import ruiner from '../../images/ruiner.png'
+import one from '../../images/fallout.png'
+import two from '../../images/lara.png'
+import three from '../../images/portal.png'
+import four from '../../images/ruiner.png'
 
 let id = 0;
 export default function Works() {
@@ -14,28 +14,42 @@ export default function Works() {
                 <p>These are my engineering projects because a quick brown fox jumps over the lazy dog. A quick brown fox jumps over the lazy dog. A quick brown fox jumps over the lazy dog</p>
             </div>
             <div className="slides">
-                <img src={fall} alt="" id="imagePan" />
+
+                <img src={one} id="imagePan1" />
+
                 <div className="buttons">
-                    <button className="button left" style={{ float: "left", }} onClick={() => { id++; changePic(); }}>⮜</button>
-                    <button className="button right" style={{ float: "right", }} onClick={() => { id--; changePic(); }}>⮞</button>
+                    <button className="button left" style={{ float: "left", }} onClick={() => { id--; changePic("imagePan1"); }}>⮜</button>
+                    <button className="button right" style={{ float: "right", }} onClick={() => { id++; changePic("imagePan1"); }}>⮞</button>
                 </div>
             </div>
         </div>
     )
 }
-let changePic = () => {
+let changePic = (imageID) => {
     if (id > 3)
         id = 0;
     else if (id < 0)
         id = 3;
+    document.querySelector("#" + imageID).style = "opacity: 5%"
+    setTimeout(() => {
+        if (id === 0) {
 
-    if (id == 0) {
-        document.querySelector("#imagePan").src = fall;
-    } else if (id == 1) {
-        document.querySelector("#imagePan").src = lara;
-    } else if (id == 2) {
-        document.querySelector("#imagePan").src = portal;
-    } else if (id == 3) {
-        document.querySelector("#imagePan").src = ruiner;
-    }
+            document.querySelector("#" + imageID).src = one;
+
+        } else if (id === 1) {
+
+            document.querySelector("#" + imageID).src = two;
+
+        } else if (id === 2) {
+
+            document.querySelector("#" + imageID).src = three;
+
+        } else if (id === 3) {
+
+            document.querySelector("#" + imageID).src = four;
+
+        }
+    }, 200)
+    setTimeout(() => document.querySelector("#" + imageID).style = "opacity: 100%", 250);
 }
+
