b92750808df6e0599d0c1b0bce2eb91a82b3553c
Sam Aripov <aripov86cm@gmail.com>
2021-10-29 21:24:19 -0400

    Changes in design, added panels, slideshow page in the works

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,7 +5,55 @@ import hangman from '../../images/hangman.png';
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+            <div className="row">
 
+                <div className="column">
+                    <div className="content" onClick={() => window.open('https://aripov-hangman.netlify.app/')}>
+                        <img src={hangman} alt="Hangman" />
+                        <h3 >The Hang Man Game</h3>
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div className="content">
+                        <img src={hangman} alt="Hangman" />
+                        <h3>The Hang Man Game</h3>
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div className="content">
+                        <img src={hangman} alt="Hangman" />
+                        <h3>The Hang Man Game</h3>
+                    </div>
+                </div>
+
+            </div>
+            <div className="row">
+
+                <div className="column">
+                    <div className="content" onClick={() => window.open('https://aripov-hangman.netlify.app/')}>
+                        <img src={hangman} alt="Hangman" />
+                        <h3 >The Hang Man Game</h3>
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div className="content">
+                        <img src={hangman} alt="Hangman" />
+                        <h3>The Hang Man Game</h3>
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div className="content">
+                        <img src={hangman} alt="Hangman" />
+                        <h3>The Hang Man Game</h3>
+                    </div>
+                </div>
+
+            </div>
+            
         </div>
     )
 }
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import "./works.scss"
+import fall from '../../images/fallout.png'
+import lara from '../../images/lara.png'
+import portal from '../../images/portal.png'
+import ruiner from '../../images/ruiner.png'
 
 export default function Works() {
     return (
         <div className="works" id="works">
+          <div className="text">
+              <h1>Engineering Projects</h1>
+              <p>These are my engineering projects because a quick brown fox jumps over the lazy dog. A quick brown fox jumps over the lazy dog. A quick brown fox jumps over the lazy dog</p>
+          </div>
+          <div className="slides">
+              <img src={fall} alt="" />
+                <div className="buttons">
+                    <button className="button" style={{float: "left",}}>⮜</button>
+                    <button className="button" style={{float: "right",}}>R</button>
+                </div>
+            </div>
         </div>
     )
 }
