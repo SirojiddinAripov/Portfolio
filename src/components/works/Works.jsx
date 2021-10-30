14d5daff2029a69ce448ff626d112189235af214
Sam Aripov <aripov86cm@gmail.com>
2021-10-29 22:28:07 -0400

    Added functionality to slides / No animations are present atm

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -5,20 +5,37 @@ import lara from '../../images/lara.png'
 import portal from '../../images/portal.png'
 import ruiner from '../../images/ruiner.png'
 
+let id = 0;
 export default function Works() {
     return (
         <div className="works" id="works">
-          <div className="text">
-              <h1>Engineering Projects</h1>
-              <p>These are my engineering projects because a quick brown fox jumps over the lazy dog. A quick brown fox jumps over the lazy dog. A quick brown fox jumps over the lazy dog</p>
-          </div>
-          <div className="slides">
-              <img src={fall} alt="" />
+            <div className="text">
+                <h1>Engineering Projects</h1>
+                <p>These are my engineering projects because a quick brown fox jumps over the lazy dog. A quick brown fox jumps over the lazy dog. A quick brown fox jumps over the lazy dog</p>
+            </div>
+            <div className="slides">
+                <img src={fall} alt="" id="imagePan" />
                 <div className="buttons">
-                    <button className="button left" style={{float: "left",}} onClick={()=> console.log("left")}>⮜</button>
-                    <button className="button right" style={{float: "right",}} onClick={()=> console.log("right")}>⮞</button>
+                    <button className="button left" style={{ float: "left", }} onClick={() => { id++; changePic(); }}>⮜</button>
+                    <button className="button right" style={{ float: "right", }} onClick={() => { id--; changePic(); }}>⮞</button>
                 </div>
             </div>
         </div>
     )
+}
+let changePic = () => {
+    if (id > 3)
+        id = 0;
+    else if (id < 0)
+        id = 3;
+
+    if (id == 0) {
+        document.querySelector("#imagePan").src = fall;
+    } else if (id == 1) {
+        document.querySelector("#imagePan").src = lara;
+    } else if (id == 2) {
+        document.querySelector("#imagePan").src = portal;
+    } else if (id == 3) {
+        document.querySelector("#imagePan").src = ruiner;
+    }
 }
